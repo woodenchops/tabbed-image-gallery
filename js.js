fb9dc659649405a8ba944fdd909d1dca2bae7f78
kristian woods <kris_woods@hotmail.co.uk>
2018-08-15 20:39:18 +0100

    added working js

--- js.js
+++ js.js
@@ -1,27 +1,36 @@
-let link = document.querySelectorAll('a');
-    let imageContainer = document.querySelector('.image-container');
-    let images = document.querySelector('.image-container img');
-    let localPath;
+var links = document.querySelectorAll('a');
+    var imageContainer = document.querySelector('.image-container');
+    var localPath;
     
     
-    let imagesName = [ // array of image names 
+    var imagesName = { // array of image names 
       
-        {
-            "album1": ["pexels-photo-412537.jpeg", "pexels-photo-748898.jpeg", "pexels-photo-1313191.jpeg"]
-        },
+            "album1": ["pexels-photo-412537.jpeg", "pexels-photo-748898.jpeg", "pexels-photo-1313191.jpeg"],
         
-        {
-            "album2": ["pexels-photo-426893.jpeg", "pexels-photo-573241.jpeg", "pexels-photo-1139370.jpeg"]
-        },
+            "album2": ["pexels-photo-426893.jpeg", "pexels-photo-573241.jpeg", "pexels-photo-1139370.jpeg"],
         
-        {
             "album3": ["pexels-photo-58625.jpeg", "pexels-photo-1308684.jpeg", "pexels-photo-1308754.jpeg"]
-        }
         
-    ];
+    };
     
     
+
+        
+    function createImg(src) { // after the deleteImages function runs, populate the .image-container with new images 
+        
+            var img = document.createElement("img");
+            img.src = src;
+            imageContainer.appendChild(img);
+    
+    }
+
+
+
+
+
     function deleteImages() { // delete any images that were in the .image-container 
+        
+        var images = document.querySelectorAll('.image-container img');
         
         if(imageContainer.childElementCount > 0) { // check if .image-container has any children - if so, delete them. 
         
@@ -31,19 +40,11 @@ let link = document.querySelectorAll('a');
             
         }
     }
+
+
     
     
-    
-    function createImg(src) { // after the deleteImages function runs, populate the .image-container with new images 
-        
-            let img = document.createElement("img");
-            img.src = src;
-            imageContainer.appendChild(img);
-    
-    }
-    
-    
-    link.forEach((link) => { // loop through <a> tags and create click event on each 
+    links.forEach((link) => { // loop through <a> tags and create click event on each 
         link.addEventListener('click', () => {
             
             switch(link.textContent) { // check name of link clicked 
@@ -51,11 +52,8 @@ let link = document.querySelectorAll('a');
     
                     deleteImages();
                     localPath = "/images/album1/"; // folder directory path to album 1
-                    for (let i = 0; i < imagesName.length; i++) {
-                        let imageURL = imagesName[i].album1; // (3) ["pexels-photo-412537.jpeg", "pexels-photo-748898.jpeg", "pexels-photo-1313191.jpeg"]
-                        for (let n = 0; n < imageURL.length; n++) {
-                            createImg(localPath + imageURL[n]); // join folder directory with image name to create img src attr 
-                        }
+                    for (let i = 0; i < imagesName.album1.length; i++) {
+                            createImg(localPath + imagesName.album1[i]); // join folder directory with image name to create img src attr 
                     }
                     
                     
@@ -63,14 +61,10 @@ let link = document.querySelectorAll('a');
                 case "image 2":
                     
                         deleteImages();
-                        localPath = "/images/album2/";
-                        for (let i = 0; i < imagesName.length; i++) {
-                            let imageURL = imagesName[i].album2; /* checked debugger - get error: "js.js:69 Uncaught TypeError: Cannot read property 'length' of undefined
-                                                                  at HTMLAnchorElement.link.addEventListener (js.js:69)"*/
-                            for (let n = 0; n < imageURL.length; n++) {
-                                createImg(localPath + imageURL[n]);
-                            }
-                        }
+                        localPath = "/images/album2/";  // folder directory path to album 2
+                    for (let i = 0; i < imagesName.album2.length; i++) {
+                            createImg(localPath + imagesName.album2[i]); // join folder directory with image name to create img src attr 
+                    }
                     
                     
                     
@@ -80,14 +74,10 @@ let link = document.querySelectorAll('a');
                 case "image 3":
     
                     deleteImages();
-                    localPath = "/images/album3/";
-                    for (let i = 0; i < imagesName.length; i++) {
-                        let imageURL = imagesName[i].album3; /* checked debugger - get error:  js.js:85 Uncaught TypeError: Cannot read property 'length' of undefined
-                                                              at HTMLAnchorElement.link.addEventListener (js.js:85)*/
-                        for (let n = 0; n < imageURL.length; n++) {
-                            createImg(localPath + imageURL[n]);
+                    localPath = "/images/album3/";  // folder directory path to album 3
+                  for (let i = 0; i < imagesName.album3.length; i++) {
+                                createImg(localPath + imagesName.album3[i]); // join folder directory with image name to create img src attr 
                         }
-                    }
                     
                     
                     
@@ -96,10 +86,3 @@ let link = document.querySelectorAll('a');
            
         });
     });
-
-
-
-
-
-
-
